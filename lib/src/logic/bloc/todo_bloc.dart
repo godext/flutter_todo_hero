d301dddeaae74018b1b6feea4d48e30a9d2d6bee
Maurice Volk <maurice@Maurices-MBP.fritz.box>
2023-01-28 13:48:42 +0100

    todo's are still not being loaded, built TodoDisplayBloc though

--- lib/src/logic/bloc/todo_bloc.dart
+++ lib/src/logic/bloc/todo_bloc.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
