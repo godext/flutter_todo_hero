c26c964a220a2949435cddc71079bdee96584031
Maurice Volk <maurice@Maurices-MBP.fritz.box>
2023-02-23 17:45:13 +0100

    added Bloc-Builder, didn't rly help

--- lib/src/screens/ToDo/home_screen.dart
+++ lib/src/screens/ToDo/home_screen.dart
@@ -42,160 +42,168 @@ class TodoList extends StatelessWidget {
       )..add(
           const TodoDisplaySubscriptionRequested(),
         ),
-      child: CupertinoPageScaffold(
-        backgroundColor: CupertinoColors.darkBackgroundGray,
-        navigationBar: CupertinoNavigationBar(
-          middle: const Text('To-Do',
-              style: TextStyle(
-                fontSize: 28,
-                color: CupertinoColors.white,
-              )),
-          backgroundColor: CupertinoColors.systemBlue,
-          leading: PopupMenuButton<String>(
-            icon: const Icon(
-              CupertinoIcons.slider_horizontal_3,
-              color: Colors.white,
+      child: BlocBuilder<TodoDisplayBloc, TodoDisplayState>(
+        buildWhen: (previous, current) => previous.filter != current.filter,
+        builder: (context, state) {
+          return CupertinoPageScaffold(
+            backgroundColor: CupertinoColors.darkBackgroundGray,
+            navigationBar: CupertinoNavigationBar(
+              middle: const Text('To-Do',
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: CupertinoColors.white,
+                  )),
+              backgroundColor: CupertinoColors.systemBlue,
+              leading: PopupMenuButton<String>(
+                icon: const Icon(
+                  CupertinoIcons.slider_horizontal_3,
+                  color: Colors.white,
+                ),
+                onSelected: (String choice) {},
+                itemBuilder: (BuildContext context) {
+                  return [
+                    PopupMenuItem<String>(
+                      value: 'All',
+                      child: _buildMenuItem(
+                        'All',
+                        context.read<TodoDisplayBloc>().state.filter,
+                        context,
+                      ),
+                      onTap: () => context.read<TodoDisplayBloc>().add(
+                            const TodoDisplayFilterSet(
+                                filter: TodoDisplayFilter.all),
+                          ),
+                    ),
+                    PopupMenuItem<String>(
+                      value: 'Active',
+                      child: _buildMenuItem(
+                        'Active',
+                        context.read<TodoDisplayBloc>().state.filter,
+                        context,
+                      ),
+                      onTap: () => context.read<TodoDisplayBloc>().add(
+                            const TodoDisplayFilterSet(
+                                filter: TodoDisplayFilter.active),
+                          ),
+                    ),
+                    PopupMenuItem<String>(
+                      value: 'Done',
+                      child: _buildMenuItem(
+                        'Done',
+                        context.read<TodoDisplayBloc>().state.filter,
+                        context,
+                      ),
+                      onTap: () => context.read<TodoDisplayBloc>().add(
+                            const TodoDisplayFilterSet(
+                                filter: TodoDisplayFilter.done),
+                          ),
+                    ),
+                  ];
+                },
+              ),
+              trailing: IconButton(
+                color: Colors.white,
+                onPressed: () {
+                  context.read<AppBloc>().add(const AppLogoutRequested());
+                },
+                icon: const Icon(CupertinoIcons.square_arrow_left),
+              ),
             ),
-            onSelected: (String choice) {},
-            itemBuilder: (BuildContext context) {
-              return [
-                PopupMenuItem<String>(
-                  value: 'All',
-                  child: _buildMenuItem(
-                    'All',
-                    context.read<TodoDisplayBloc>().state.filter,
-                    context,
-                  ),
-                  onTap: () => context.read<TodoDisplayBloc>().add(
-                        const TodoDisplayFilterSet(
-                            filter: TodoDisplayFilter.all),
-                      ),
-                ),
-                PopupMenuItem<String>(
-                  value: 'Active',
-                  child: _buildMenuItem(
-                    'Active',
-                    context.read<TodoDisplayBloc>().state.filter,
-                    context,
-                  ),
-                  onTap: () => context.read<TodoDisplayBloc>().add(
-                        const TodoDisplayFilterSet(
-                            filter: TodoDisplayFilter.active),
-                      ),
-                ),
-                PopupMenuItem<String>(
-                  value: 'Done',
-                  child: _buildMenuItem(
-                    'Done',
-                    context.read<TodoDisplayBloc>().state.filter,
-                    context,
-                  ),
-                  onTap: () => context.read<TodoDisplayBloc>().add(
-                        const TodoDisplayFilterSet(
-                            filter: TodoDisplayFilter.done),
-                      ),
-                ),
-              ];
-            },
-          ),
-          trailing: IconButton(
-            color: Colors.white,
-            onPressed: () {
-              context.read<AppBloc>().add(const AppLogoutRequested());
-            },
-            icon: const Icon(CupertinoIcons.square_arrow_left),
-          ),
-        ),
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                child: BlocBuilder<TodoDisplayBloc, TodoDisplayState>(
-                  builder: (context, state) {
-                    TodoDisplayBloc displayBloc =
-                        BlocProvider.of<TodoDisplayBloc>(context);
-                    return ListView(
-                      //physics: const BouncingScrollPhysics(),
-                      physics: const BouncingScrollPhysics(
-                          parent: AlwaysScrollableScrollPhysics()),
-                      children: [
-                        for (final todo in state.allTodos)
-                          Dismissible(
-                            key: Key(todo.id!),
-                            direction: DismissDirection.horizontal,
-                            onDismissed: (direction) {
-                              if (direction == DismissDirection.startToEnd) {
-                                displayBloc
-                                    .add(TodoDisplayDismissed(todo: todo));
-                              }
+            child: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: BlocBuilder<TodoDisplayBloc, TodoDisplayState>(
+                      builder: (context, state) {
+                        TodoDisplayBloc displayBloc =
+                            BlocProvider.of<TodoDisplayBloc>(context);
+                        return ListView(
+                          //physics: const BouncingScrollPhysics(),
+                          physics: const BouncingScrollPhysics(
+                              parent: AlwaysScrollableScrollPhysics()),
+                          children: [
+                            for (final todo in state.allTodos)
+                              Dismissible(
+                                key: Key(todo.id!),
+                                direction: DismissDirection.horizontal,
+                                onDismissed: (direction) {
+                                  if (direction ==
+                                      DismissDirection.startToEnd) {
+                                    displayBloc
+                                        .add(TodoDisplayDismissed(todo: todo));
+                                  }
 
-                              if (direction == DismissDirection.endToStart) {
-                                displayBloc.add(TodoDisplayCompletionToggled(
+                                  if (direction ==
+                                      DismissDirection.endToStart) {
+                                    displayBloc
+                                        .add(TodoDisplayCompletionToggled(
+                                      todo: todo,
+                                      isCompleted: !todo.isCompleted,
+                                    ));
+                                  }
+                                },
+                                background: Container(
+                                  color: Colors.red,
+                                  alignment: Alignment.centerLeft,
+                                  child: const Icon(
+                                    Icons.delete,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                secondaryBackground: Container(
+                                  color: Colors.green,
+                                  alignment: Alignment.centerRight,
+                                  child: const Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                child: TodoListTile(
                                   todo: todo,
-                                  isCompleted: !todo.isCompleted,
-                                ));
-                              }
-                            },
-                            background: Container(
-                              color: Colors.red,
-                              alignment: Alignment.centerLeft,
-                              child: const Icon(
-                                Icons.delete,
-                                color: Colors.white,
-                              ),
-                            ),
-                            secondaryBackground: Container(
-                              color: Colors.green,
-                              alignment: Alignment.centerRight,
-                              child: const Icon(
-                                Icons.done,
-                                color: Colors.white,
-                              ),
-                            ),
-                            child: TodoListTile(
-                              todo: todo,
-                              leading: todo.isCompleted == true
-                                  ? iconTodoFinished
-                                  : iconTodoUnfinished,
-                              onTapLeading: () => displayBloc.add(
-                                TodoDisplayCompletionToggled(
-                                  todo: todo,
-                                  isCompleted: !todo.isCompleted,
+                                  leading: todo.isCompleted == true
+                                      ? iconTodoFinished
+                                      : iconTodoUnfinished,
+                                  onTapLeading: () => displayBloc.add(
+                                    TodoDisplayCompletionToggled(
+                                      todo: todo,
+                                      isCompleted: !todo.isCompleted,
+                                    ),
+                                  ),
+                                  onTapTodo: () => Navigator.of(context).push(
+                                    TodoEditPage.route(todo),
+                                  ),
                                 ),
                               ),
-                              onTapTodo: () => Navigator.of(context).push(
-                                TodoEditPage.route(todo),
-                              ),
-                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: CupertinoButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const TodoEditPage(),
                           ),
-                      ],
-                    );
-                  },
-                ),
+                        );
+                      },
+                      color: CupertinoColors.systemBlue.withOpacity(0.7),
+                      borderRadius: BorderRadius.circular(20),
+                      child: const Text('Add Todo',
+                          style: TextStyle(
+                            color: CupertinoColors.white,
+                          )),
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: CupertinoButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const TodoEditPage(),
-                      ),
-                    );
-                  },
-                  color: CupertinoColors.systemBlue.withOpacity(0.7),
-                  borderRadius: BorderRadius.circular(20),
-                  child: const Text('Add Todo',
-                      style: TextStyle(
-                        color: CupertinoColors.white,
-                      )),
-                ),
-              ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
